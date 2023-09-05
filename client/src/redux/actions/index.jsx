08691178a4f0f8e8f85a6c58de372c203c9fbc83
facumancuso <105766388+facumancuso@users.noreply.github.com>
2023-09-05 18:39:08 -0300

    Cambios filtro por marca y categoria

--- client/src/redux/actions/index.jsx
+++ client/src/redux/actions/index.jsx
@@ -33,3 +33,55 @@ export const getAllProducts = () => {
       return errorMessage;
     };
   };
+
+  export const filterByBrand = (brandId) => {
+    return async function (dispatch) {
+      try {
+        const response = await axios.get(`http://localhost:3001/products/brands/${brandId}`);
+        dispatch({ type: GET_PODUCT_SUCCESS, payload: response.data });
+      } catch (error) {
+        dispatch({ type: ERROR, payload: 'Error al filtrar por marca' });
+      }
+    };
+  };
+  
+  export const filterByCategory = (categoryId) => {
+    return async function (dispatch) {
+      try {
+        const response = await axios.get(`http://localhost:3001/products/categories/${categoryId}`);
+        dispatch({ type: GET_PODUCT_SUCCESS, payload: response.data });
+      } catch (error) {
+        dispatch({ type: ERROR, payload: 'Error al filtrar por categoría' });
+      }
+    };
+  };
+  
+  export const getCategories = () =>{
+    return async function(dispatch){
+      try{
+        const response = await axios.get(`http://localhost:3001/products/categories/`);
+        console.log(response);
+        return response.data
+      } catch (error){
+        console.log(error);
+      }
+    }
+  }
+
+  export const getBrands = () =>{
+    return async function(dispatch){
+      try{
+        const response = await axios.get(`http://localhost:3001/products/brands/`);
+        console.log(response);
+        return response.data
+      } catch (error){
+        console.log(error);
+      }
+    }
+  }
+  
+  export const resetFilters = () => {
+    return async function (dispatch) {
+      dispatch(getAllProducts());
+    };
+  };
