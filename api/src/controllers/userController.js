5015bab13cd46c68ee45da376ec4b26cce052818
Yeison Valdeblanquez <yeival@gasguajira.local>
2023-09-04 21:12:09 -0500

    Funciones de ruta User

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -1,14 +1,28 @@
 const { user } = require('../db');
 
-// const getProductBySKU = async (sku) => {
-//   const product = await Product.findByPk(sku);
-//   if (product) {
-//     return product;
-//   } else {
-//     throw new Error('Product not found');
-//   }
-// };
-// console.log("LLego acá");
+const createUser = async (user_name,first_name, last_name,gender,email,delivery_address,country,CustomElementRegistry,mobile,role,user_status, purchase_history,user_password) => {
+  try {
+    const newUser = await user.create({
+      user_name,
+      first_name, 
+      last_name,
+      gender,
+      email,
+      delivery_address,
+      country,
+      CustomElementRegistry,
+      mobile,
+      role,
+      user_status, 
+      purchase_history,
+      user_password
+    });
+    return newUser;
+  } catch (error) {
+    throw new Error('Error creating users: ' + error.message);
+  }
+};
+
 const getAllUsers = async (req, res) => {
   
  try {
@@ -18,110 +32,29 @@ const getAllUsers = async (req, res) => {
     throw new Error('Server error, could not get the users');
   }
 
-}
+};
 
-// const getUsersById = async (req, res) => {
-//   user.findAll()
-//   .then((data) => {
-//     res.json(data);
-//   })
-//   .catch(() => {
-//     res.json({ error: error.message});
-//   });
-
-
-//   const { id } = req.params;
-//   const project = await user.findByPk(id);
-//   if (project === null) {
-//     res.status(404).json({ error: "No existe el usuario" });
-//   } else {
-//     res.json(data);
-//   }
-
-// }
-
-
-
-
-// const getProductsByBrand = async (id_brand) => {
-//   try {
-//     const productsByBrand = await Product.findAll({ where: { id_brand } });
-//     if (productsByBrand.length > 0) {
-//       return productsByBrand;
-//     } else {
-//       throw new Error('Products by brand not found');
-//     }
-//   } catch (error) {
-//     throw new Error('Server Error, could not get products by brand');
-//   }
-// };
-
-// const getBrands = async () => {
-//   try {
-//     const brandsArray = await Brand.findAll();
-//     if (brandsArray.length > 0) {
-//       return brandsArray;
-//     } else {
-//       throw new Error('Brands not found');
-//     }
-//   } catch (error) {
-//     throw new Error('Server Error, could not get brands');
-//   }
-// };
-
-// const getProductsByCategory = async (id_category) => {
-//   try {
-//     const productsByCategory = await Product.findAll({ where: { id_category } });
-//     if (productsByCategory.length > 0) {
-//       return productsByCategory;
-//     } else {
-//       throw new Error('Products by category not found');
-//     }
-//   } catch (error) {
-//     throw new Error('Server Error, could not get products by brand');
-//   }
-// }
-
-// const getCategories = async () => {
-//   try {
-//     const categoriesArray = await Category.findAll()
-//     if (categoriesArray.length > 0) {
-//       return categoriesArray;
-//     } else {
-//       throw new Error('Categories not found');
-//     }
-//   } catch (error) {
-//     throw new Error('Server Error, could not get categories');
-//   }
-// }
-
-const createUser = async (user_name, first_name, last_name, gender, email, delivery_address, country, CustomElementRegistry, user_status, purchase_history) => {
-  try {
-    const newUser = await user.create({
-      user_name,
-      first_name, 
-      last_name, 
-      gender, 
-      email, 
-      delivery_address, 
-      country, 
-      CustomElementRegistry, 
-      user_status, 
-      purchase_history,
-    });
-    return newUser;
-  } catch (error) {
-    throw new Error('Error creating users: ' + error.message);
+const getUserById = async (id) => {
+  const userId = await user.findByPk(id);
+  if (userId) {
+    return userId;
+  } else {
+    throw new Error('User not found');
   }
 };
 
-module.exports = { getAllUsers, createUser};
+const updateUsers = async (userId, newPassword ) => {
 
-// module.exports = {
-//     getAllUsers: (req, res) => {
-//       res.status(200).json("Ruta Get All Users");
+try {
+      const userUpdate = await user.update(
+        { user_password: newPassword },
+        { where: {id: userId} }
+        );
+ return userUpdate;
+} 
+catch (error) { throw new Error('Error updating users: ' + error.message);
+}
+ 
+};
 
-    
-
-//     }
-//   };
+module.exports = { getAllUsers, createUser, getUserById, updateUsers};
--- api/src/handlers/userHandler.js
+++ api/src/handlers/userHandler.js
@@ -1,6 +1,18 @@
 const userController = require('../controllers/userController');
 
 module.exports = {
+
+  createUser: async (req, res) => {
+    console.log(req.body)
+    const { user_name,first_name, last_name,gender,email,delivery_address,country,CustomElementRegistry,mobile,role,user_status, purchase_history,user_password } = req.body;
+    try {
+      const newUser = await userController.createUser(user_name,first_name, last_name,gender,email,delivery_address,country,CustomElementRegistry,mobile,role,user_status, purchase_history,user_password);
+      res.status(200).json(newUser);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
   getAllUsers: async(req, res) => {
     try {
       const users = await  userController.getAllUsers(req, res);
@@ -8,5 +20,31 @@ module.exports = {
     } catch (error) {
       res.status(500).json({ error: error.message });
     }       
+  },
+
+getUserById: async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  try {
+    const userId = await userController.getUserById(id);
+    res.status(200).json(userId);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-};
+},
+
+updateUsers: async (req, res) => {
+  // console.log(req.body)
+  const id = req.params.id;
+  const { user_password } = req.body;
+console.log("iD= " + id);
+console.log("iD2= " + user_password);
+  try {
+    const updateUser = await userController.updateUsers(id, user_password );
+    res.status(200).json(updateUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+},
+
+}
