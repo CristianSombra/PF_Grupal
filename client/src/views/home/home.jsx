b885c97865ebb454544818a6de1e5cdc039dec97
vavg89 <109152724+vavg89@users.noreply.github.com>
2023-09-06 18:14:37 -0300

    funcionando

--- client/src/views/home/home.jsx
+++ client/src/views/home/home.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardsContainer from "../../components/cardscontainer/cardscontainer";
+import Filter from "../../components/filter/filter";
 import styles from "./home.module.css";
 import banner from "../../assets/banner.png";
 import SortDivs from "../../components/SortDivs/SortDivs"
 import { useDispatch } from "react-redux";
 import { getCategories, getBrands } from "../../redux/actions";
 
-
 export default function Home() {
+  const [listCategories, setListCategories] = useState([]);
+  const [listBrands, setListBrands] = useState([]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchData = async () => {
+      const categories = await dispatch(getCategories());
+
+      setTimeout(() => {
+        setListCategories(categories);
+      }, 1000);
+      const brands = await dispatch(getBrands());
+
+      setTimeout(() => {
+        setListBrands(brands);
+      }, 1000);
+    };
+    fetchData();
+  }, [dispatch]);
 
   return (
     <div className={styles.homeContainer}>
@@ -18,7 +36,6 @@ export default function Home() {
       </div>
       <div className={styles.cardContainer}>  
        <CardsContainer />
-
       </div>
     </div>
   );
