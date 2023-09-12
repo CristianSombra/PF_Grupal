210ed52632326e2efebf8007b45c76d085970298
facumancuso <105766388+facumancuso@users.noreply.github.com>
2023-09-12 16:08:48 -0300

    Carro

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -5,25 +5,23 @@ import Button from 'react-bootstrap/Button';
 import './card.css';
 
 const Cards = (props) => {
-  const { sku } = props;
-
+  const { sku, name, image, titulo, price } = props;
 
   return (
     <Card className="custom-shadow custom-card">
       <div className="imgContainer">
-        <Card.Img variant="top" src={props.image} alt={props.titulo} />
+        <Card.Img variant="top" src={image} alt={titulo} />
       </div>
       <Card.Body className="text-center d-flex flex-column">
-        <Card.Text className="titleCard">{props.name}</Card.Text>
+        <Card.Text className="titleCard">{name}</Card.Text>
         <Card.Title className="mb-4">$ {new Intl.NumberFormat("de-DE", {
           style: "currency",
           currency: "COP",
-        }).format(props.price)}</Card.Title>
+        }).format(price)}</Card.Title>
         <div className="mt-3 text-center">
           <Button variant="dark" as={Link} to={`/detail/${sku}`}>
             Ver detalles
           </Button>
-
         </div>
       </Card.Body>
     </Card>
